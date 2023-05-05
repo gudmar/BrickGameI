01028b7f90ab1f30e1efb9314742a093718c2be7
Marek Gudalewicz <marek.gudalewicz@localhost>
2023-05-06 01:13:39 +0200

    Implementatoin of track blueprint creator ongoing

--- src/cartridges/Race/TrackGenerator.ts
+++ src/cartridges/Race/TrackGenerator.ts
@@ -1,11 +1,90 @@
+import { getRandom } from "../../functions/getRandom";
+import { GameCreator } from "../GameCreator";
 
-export class TrakcGenerator{
+const INITIAL_PROBABILITY_LEVEL_CHANGES = 10;
 
-    generateBlueprint(length: number) {
+export enum Sites {LEFT, RIGHT}
 
+export class TrackGenerator{
+
+    site: Sites;
+    distance: number;
+    lastBlueprint: number[][];
+    gamePhase;
+    testRandomValue?: number;
+    visitedObject: GameCreator;
+
+    constructor({
+        testRandomValue,
+        visitedObject,
+    }: {
+        testRandomValue?: number,
+        visitedObject: GameCreator
+    }){
+        this.visitedObject = visitedObject;
+        this.headTail = testRandomValue;
+        this.site = this.getInitialSite();
+        this.gamePhase = 0;
+        this.lastBlueprint = this.getInitialBlueprint();
+        this.distance = 0;
+    }
+
+    get headTail() { return this.testRandomValue || getRandom(0, 1)}
+    set headTail(val: number|undefined) { this.testRandomValue = val}
+
+    updateTrackIfNeeded() {
+        const shouldUpdate = this.gamePhase % 9 === 0 && this.gamePhase !== 0;
+        if (!shouldUpdate) return;
+        const nextTrackBit = this.getNextBlueprintBit();
+        const [, middle, last] = this.lastBlueprint;
+        this.lastBlueprint = [middle, last, nextTrackBit]
+    }
+
+    shouldSiteChange() {
+        const {level} = this.visitedObject;
+        const randomValue = getRandom(0, INITIAL_PROBABILITY_LEVEL_CHANGES - level);
+        return randomValue === 0;
+    }
+
+    changeSiteIfNeeded(){
+        const shouldSwitchSide = this.shouldSiteChange();
+        if (shouldSwitchSide) {
+            this.site = this.site === Sites.RIGHT ? Sites.LEFT : Sites.RIGHT;
+        }
+    }
+
+    getInitialBlueprint() {
+        const result = this.site === Sites.RIGHT ? [
+            [0, 0],[0, 0],[0, 1]
+        ] : [
+            [0, 0],[0, 0],[1, 0]
+        ]        
+        return result;
+    }
+
+    getInitialSite() {
+        return this.headTail === 0 ? Sites.LEFT : Sites.RIGHT
+    }
+
+    getNextBlueprintBit() {
+        this.changeSiteIfNeeded();
+        return this.site === Sites.LEFT ? [1, 0] : [0, 1]
+    }
+
+    toggleSite() {
+        this.site = this.site === Sites.LEFT ? Sites.RIGHT : Sites.LEFT;
+    }
+
+    generateBlueprint() {
+        this.updateTrackIfNeeded();
+        return this.lastBlueprint;
     }
 
     renderTrackBit(trackBit: number[][]){
+
+    }
+
+    renderBoard(track: number[][]){
 
     }
     
---
+++ src/cartridges/Race/__tests__/TrackGenerator.test.ts
@@ -0,0 +1,61 @@
+import { GameCreator } from "../../GameCreator";
+import { Sites, TrackGenerator } from "../TrackGenerator";
+
+describe('Testing track generator', () => {
+    describe('Testing generateBlueprint', () => {
+        const visitedObject = {level: 0};
+        beforeEach(() => {
+            visitedObject.level = 0;
+        })
+        it('Should generate [0 0], [0, 0], [0, 1] when game started and initial site is randomly chosen to RIGHT', () => {
+            const generator = new TrackGenerator({testRandomValue: Sites.RIGHT, visitedObject: visitedObject as GameCreator});
+            const result = generator.generateBlueprint();
+            expect(result).toEqual([[0, 0], [0, 0], [0, 1]])
+        });
+        it('Should generate [0 0], [0, 0], [1, 0] when game started and initial site is randomly chosen to LEFT', () => {
+            const generator = new TrackGenerator({testRandomValue: Sites.LEFT, visitedObject: visitedObject as GameCreator});
+            const result = generator.generateBlueprint();
+            expect(result).toEqual([[0, 0], [0, 0], [1, 0]])
+        })
+        it('Should not generate new entry if PHASE is < 9', () => {
+            // phase is step of car takeover: car is 4 long, space left must be 5 long
+            // Generation is lasy,
+            const generator = new TrackGenerator({testRandomValue: Sites.RIGHT, visitedObject: visitedObject as GameCreator});
+            generator.gamePhase = 8;
+            const result = generator.generateBlueprint();
+            expect(result).toEqual([[0, 0], [0, 0], [0, 1]])
+        })
+        it('Should generate 00 10 10 if left 1 game phase and no change in direction given', () => {
+            jest.mock('../../../functions/getRandom', () => ({
+                getRandom: () => 1
+            }))
+
+            const generator = new TrackGenerator({testRandomValue: Sites.RIGHT, visitedObject: visitedObject as GameCreator});
+            // generator.headTail = Sites.RIGHT;
+            // generator.site = Sites.RIGHT;
+            // generator.lastBlueprint = generator.getInitialBlueprint();
+            generator.gamePhase = 9;
+            
+            const result = generator.generateBlueprint();
+            expect(result).toEqual([[0, 0], [0, 1], [0, 1]])
+        })
+        it('Should generate 00 10 01 if left 1 game phase and change in direction given', () => {
+
+        })
+
+    });
+    describe('Testing renderTrackBit', () => {
+        it('Should render 0 0', () => {
+
+        })
+        it('Should render 0 1', () => {
+
+        })
+        it('Should render 1 0', () => {
+            
+        })
+    })
+    describe('Testing renderBoard', () => {
+        // it('Should ')
+    })
+})
