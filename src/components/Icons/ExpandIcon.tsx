a70542725ba00252af908b413dec7cdef4f332f2
Marek Gudalewicz <marek.gudalewicz@localhost>
2023-06-30 01:15:23 +0200

    Changes to navigation and svg made

--- src/components/Icons/ExpandIcon.tsx
+++ src/components/Icons/ExpandIcon.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import styles from './style.module.css';
-import ExpandWhite from '../../IconImages/ExpandWhite.svg';
-import ExpandBlack from '../../IconImages/ExpandBlack.svg';
+import {ReactComponent as ExpandWhite} from '../../IconImages/ExpandWhite.svg';
+import {ReactComponent as ExpandBlack} from '../../IconImages/ExpandBlack.svg';
 
 
 function ExpandIcon({variant='black'}: {variant: 'black'|'white'}) {
     const isWhite = variant === 'white';
   return (
     <span className={styles.icon}>
-        <img
+      {isWhite ? <ExpandWhite/> : <ExpandBlack/>}
+        {/* <img
           src={ isWhite ? ExpandWhite : ExpandBlack }
           alt=""
-        />
+        /> */}
     </span>
   )
 }
--- src/components/Icons/SoundOnIcon.tsx
+++ src/components/Icons/SoundOnIcon.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import styles from './style.module.css';
-import SoundHidden from '../../IconImages/SoundHidden.svg';
-import SoundVisible from '../../IconImages/SoundVisible.svg';
+import { ReactComponent as SoundHidden } from '../../IconImages/SoundHidden.svg';
+import { ReactComponent as SoundVisible } from '../../IconImages/SoundVisible.svg';
 
 
 function SoundIcon({variant='On'}: {variant: 'On'|'Off'}) {
     const isOn = variant === 'On';
   return (
     <span className={styles.icon}>
-        <img
+      { isOn ? <SoundVisible/> : <SoundHidden/> }
+        {/* <img
           src={ isOn ? SoundVisible : SoundHidden }
           alt=""
-        />
+        /> */}
     </span>
   )
 }
